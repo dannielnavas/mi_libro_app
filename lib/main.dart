09bc31e85669b8c59a9673f99e4af1110ba48d96
Danniel Navas <dannielnavas@gmail.com>
2025-01-13 07:56:14 -0500

    Agregar diseño de tarjeta para mostrar información del libro en la pantalla principal

--- lib/main.dart
+++ lib/main.dart
@@ -20,12 +20,53 @@ class MiLibroApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: Text(
-            'Mi libro app',
-            style: TextStyle(color: Colors.white),
-          )),
-    );
+        appBar: AppBar(
+            backgroundColor: Colors.green,
+            title: Text(
+              'Mi libro app',
+              style: TextStyle(color: Colors.white),
+            )),
+        body: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: 125,
+            child: Card(
+              child: Row(
+                children: <Widget>[
+                  SizedBox(
+                    height: 125,
+                    width: 100,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.network(
+                        'https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        'The Lean Startup',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        'Eric Ries',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Text(
+                        '2011',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Container(
+                        height: 2,
+                        width: 75,
+                        color: Colors.green,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )));
   }
 }
