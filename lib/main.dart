9a899cf6ac81ef06408a7fff35a0756db601909f
Danniel Navas <dannielnavas@gmail.com>
2025-01-13 08:07:59 -0500

    Agregar pestañas para navegación entre libros y autores, y crear pantalla de inicio para mostrar información del libro

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mi_libro_app/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,54 +20,36 @@ class MiLibroApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-            backgroundColor: Colors.green,
-            title: Text(
-              'Mi libro app',
-              style: TextStyle(color: Colors.white),
-            )),
-        body: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: 125,
-            child: Card(
-              child: Row(
-                children: <Widget>[
-                  SizedBox(
-                    height: 125,
-                    width: 100,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: Image.network(
-                        'https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg',
-                        fit: BoxFit.cover,
-                      ),
-                    ),
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.green,
+              title: Text(
+                'Mi libro app',
+                style: TextStyle(color: Colors.white),
+              ),
+              bottom: TabBar(
+                indicatorColor: Colors.white,
+                labelColor: Colors.white,
+                unselectedLabelColor: Colors.white,
+                tabs: <Widget>[
+                  Tab(
+                    icon: Icon(Icons.book),
+                    text: 'Libros',
                   ),
-                  SizedBox(width: 20),
-                  Column(
-                    children: <Widget>[
-                      Text(
-                        'The Lean Startup',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Text(
-                        'Eric Ries',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      Text(
-                        '2011',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      Container(
-                        height: 2,
-                        width: 75,
-                        color: Colors.green,
-                      )
-                    ],
+                  Tab(
+                    icon: Icon(Icons.person),
+                    text: 'Autores',
                   )
                 ],
               ),
-            )));
+            ),
+            body: TabBarView(children: [
+              HomeScreen(),
+              Center(
+                child: Text('Autores'),
+              )
+            ])));
   }
 }
